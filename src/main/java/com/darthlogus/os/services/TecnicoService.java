9e8406675753a8150a306240e3129eb809f86c86
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-02-09 15:37:54 -0300

    Tratamento de exceptions.

--- src/main/java/com/darthlogus/os/services/TecnicoService.java
+++ src/main/java/com/darthlogus/os/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.darthlogus.os.services;
 
 import com.darthlogus.os.domain.Tecnico;
 import com.darthlogus.os.repositories.TecnicoRepository;
+import com.darthlogus.os.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "+id+", Tipo: "+Tecnico.class.getName()));
     }
 }
